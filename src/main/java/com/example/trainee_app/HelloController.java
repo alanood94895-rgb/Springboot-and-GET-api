175cb9c23feaa2bb27205b5e695a0d10d8bb9591
CompanyProfile <alanood94895@gmail.com>
2026-06-02 14:41:04 +0400

    Serving Personalized Dynamic Greetings

--- src/main/java/com/example/trainee_app/HelloController.java
+++ src/main/java/com/example/trainee_app/HelloController.java
@@ -2,12 +2,17 @@ package com.example.trainee_app;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-@RestController
 
+@RestController
 public class HelloController {
 
-        @GetMapping("/hello")
-        public String welcomeMessage() {
-            return "Welcome to Code Creators Spring Boot Training!";
-        }
+    @GetMapping("/hello")
+    public String welcomeMessage() {
+        return "Welcome to Code Creators Spring Boot Training!";
+    }
+
+    @GetMapping("/help")
+    public String helpMessage() {
+        return "If you are stuck, please post your error screenshot on Slack!";
+    }
 }
